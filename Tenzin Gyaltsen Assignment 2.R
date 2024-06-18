dd0c68b05a6a46bbdac5372ab0c7a052d0a61cce
Tenzin Gyaltsen <tenzingyaltsen16@gmail.com>
2024-06-18 15:33:35 -0400

    Completed steps 2 and 3.

--- Tenzin Gyaltsen Assignment 2.R
+++ Tenzin Gyaltsen Assignment 2.R
@@ -7,3 +7,23 @@
 #' 4. Check if the number is narcissistic.
 #' 5. Display the result with an appropriate message.
 
+#' 1. Prompt user to enter a 3 digit answer which is saved into the variable
+#' "answer". 
+answer_original <- readline(prompt = "Please enter a 3 digit positive number: ")
+
+#' 2. Check if the input is numeric by first forcing the input to numeric
+#' type using the "as.numeric()" function. If the value becomes NA then the
+#' input is not numeric. This can be checked using an if statement and the 
+#' "is.na()" function, with appropriate error message displayed if true.
+answer_numeric <- as.numeric(answer_original)
+answer_numeric
+
+if (is.na(answer_numeric)) {
+  paste(answer_original, "is not numeric.")
+} else if (answer_numeric < 99 || answer_numeric > 999 || answer_numeric%%1 != 0) {
+  #' 3. Check if the input is not positive or not 3 digits (i.e., less
+  #' than 99 and more than 999). Also check that the input is a whole number 
+  #' (no decimals) by confirming that the input remainder is 0 when divided by 1.
+  #' If true, then print an appropriate error message.
+  paste(answer_original, "is not a positive 3 digit whole number.")
+} 

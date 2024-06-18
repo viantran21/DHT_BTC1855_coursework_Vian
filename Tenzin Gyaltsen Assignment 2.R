6efd4e86b5057c3a36ac8443fc6314bdf9198a15
Tenzin Gyaltsen <tenzingyaltsen16@gmail.com>
2024-06-18 14:03:06 -0400

    Initial commit of assignment 2 with rough plan.

--- Tenzin Gyaltsen Assignment 2.R
+++ Tenzin Gyaltsen Assignment 2.R
@@ -1,1 +1,9 @@
-# Test commit.
+# Assignment 2.
+
+#' Rough Plan:
+#' 1. Prompt the user to enter a 3 digit positive number.
+#' 2. Check if the input is numeric.
+#' 3. Check if the input is positive and 3 digits.
+#' 4. Check if the number is narcissistic.
+#' 5. Display the result with an appropriate message.
+

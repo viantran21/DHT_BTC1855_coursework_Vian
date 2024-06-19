c6948de62f0c49dcacdaabd46a9735d5a50b0cbe
Tenzin Gyaltsen <tenzingyaltsen16@gmail.com>
2024-06-19 12:48:31 -0400

    Added documentation for Steps 4 and 5 and added a Purpose section at the top of the script.

--- Tenzin Gyaltsen Assignment 2.R
+++ Tenzin Gyaltsen Assignment 2.R
@@ -24,17 +24,26 @@ if (is.na(answer_numeric)) {
 } else if (answer_numeric < 99 || answer_numeric > 999 || answer_numeric%%1 != 0) {
   #' 3. Check if the input is not positive or not 3 digits (i.e., less
   #' than 99 and more than 999). Also check that the input is a whole number 
-  #' (no decimals) by confirming that the input remainder is 0 when divided by 1.
-  #' If true, then print an appropriate error message.
+  #' (no decimals) by confirming that the input remainder is 0 when divided 
+  #' by 1. If true, then print an appropriate error message.
   paste(answer_original, "is not a positive 3 digit whole number.")
 } else {
+  #' 4. Check if the number is an Armstrong number by first extracting each 
+  #' digit character, converting them to numerics, and saving them into 
+  #' separate variables. Then sum the digits and assign the value to a new 
+  #' variable called "digit_sum". Create a nested if statement to evaluate
+  #' whether the sum of the digits is equal to the user's numerical input.
   digit1 <- as.numeric(substring(answer_original, 1, 1))
   digit2 <- as.numeric(substring(answer_original, 2, 2))
   digit3 <- as.numeric(substring(answer_original, 3, 3))
   digit_sum <- digit1^3 + digit2^3 + digit3^3
   if (digit_sum == answer_numeric) {
+    #' 5. If the sum of the digits is equal to the user's number, display
+    #' an appropriate message.
     paste(answer_original, "is an Armstrong number!")
   } else {
+    #' 5. If the sum of the digits is not equal to the user's number, 
+    #' display an appropriate message.
     paste(answer_original, "is not an Armstrong number.")
   }
 }
